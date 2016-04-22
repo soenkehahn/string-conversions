46306ab7015cb1c035b6b1e901bb7f86532a41c2
Sönke Hahn <soenkehahn@gmail.com>
2016-04-22 13:43:12 +0800

    added test-suite

---
+++ test/Data/String/ConversionsSpec.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE ViewPatterns #-}
+
+module Data.String.ConversionsSpec where
+
+import           Control.DeepSeq
+import           Test.Hspec
+import           Test.QuickCheck
+import           Test.QuickCheck.Instances ()
+
+import           Data.String.Conversions
+
+spec :: Spec
+spec = do
+  describe "cs" $ do
+    it "allows to convert from String to strict Text" $ do
+      property $ \ s ->
+        show (s :: String) `shouldBe` show (cs s :: ST)
+
+    context "when converting from strict ByteString to strict Text" $ do
+      it "never crashes" $ do
+        property $ \ s ->
+          deepseq (cs (s :: SBS) :: ST) (return () :: IO ())
